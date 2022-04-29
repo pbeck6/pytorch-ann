a5dd844cb800bfe3aa2510f2e0c0f20b8b48cf26
stoneroll6 <stoneroll6@gmail.com>
2022-04-29 22:07:32 +0900

    Geospatial functions working

--- full_ann.py
+++ full_ann.py
@@ -4,27 +4,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Get tabular neural net Model
+# Get tabular neural net Model and haversine dist func
 from neuralnet.models import TabularModel
-
-def haversine_distance(df, lat1, long1, lat2, long2):
-    """
-    Calculates the haversine distance between 2 sets of GPS coordinates in df
-    https://en.wikipedia.org/wiki/Haversine_formula
-    """
-    r = 6371  # Earth's average radius (km)
-       
-    phi1 = np.radians(df[lat1])
-    phi2 = np.radians(df[lat2])
-    
-    delta_phi = np.radians(df[lat2]-df[lat1])
-    delta_lambda = np.radians(df[long2]-df[long1])
-     
-    a = np.sin(delta_phi/2)**2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda/2)**2
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
-    d = (r * c) # (km)
-
-    return d
+from geo.haversine import haversine_distance
 
 if __name__ == "__main__":
     # Read in dataset of taxi fares
